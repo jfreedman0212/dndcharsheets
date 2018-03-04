d2306fc55ac133c1e13e384a5f97a2ea761d708e
Tarun Belani <tbelani@ufl.edu>
2018-03-03 21:42:54 -0500

    add characters

--- DND_Char_Sheet/AppDelegate.swift
+++ DND_Char_Sheet/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UINavigationBar.appearance().backgroundColor = UIColor.init(red: 179/255, green: 61/255, blue: 57/255, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.black
+        UINavigationBar.appearance().isOpaque = true
+        
         return true
     }
 
---
+++ DND_Char_Sheet/CharacterAddViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CharacterAddViewController.swift
+//  DND_Char_Sheet
+//
+//  Created by T on 3/3/18.
+//  Copyright © 2018 Josh Freedman. All rights reserved.
+//
+
+import UIKit
+
+class CharacterAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        viewWillDisappear(true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
