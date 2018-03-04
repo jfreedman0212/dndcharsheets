122b406ada46bb68eb6ecd8e513b9871c500bf55
Tarun Belani <tbelani@ufl.edu>
2018-03-04 02:48:40 -0500

    stats

---
+++ DND_Char_Sheet/StatsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  StatsViewController.swift
+//  DND_Char_Sheet
+//
+//  Created by T on 3/4/18.
+//  Copyright © 2018 Josh Freedman. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.tintColor = UIColor.black
+        self.navigationController?.view.backgroundColor = .clear
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
