3dc1d6d9910d95b32c1e0835351b12624a350961
Tarun Belani <tbelani@ufl.edu>
2018-03-04 04:34:01 -0500

    test

--- DND_Char_Sheet/MainViewController.swift
+++ DND_Char_Sheet/MainViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    //GetProperty(String: a)
+    
     @IBOutlet weak var scrollView: UIScrollView!
     var characters: [String] = [String]()
     
@@ -18,13 +20,19 @@ class MainViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         viewWillAppear(false)
-        
+        viewDidAppear(true)
+    
+        }
+    
+    override func viewDidAppear(_ animated: Bool) {
         let xConst = 41.0
         let yInit = 94.0
         scrollView.contentSize = CGSize(width: 294, height: 444 + ((Double(characters.count) - 8) * 55.5))
-        for index in 0...characters.count - 1 {
-            let button = UIButton(frame: CGRect(x: xConst, y: yInit + (Double(index) * 55.5), width: 294.0, height: 55.5))
+        if characters.count > 0 {
+            for index in 0...characters.count - 1 {
+                let button = UIButton(frame: CGRect(x: xConst, y: yInit + (Double(index) * 55.5), width: 294.0, height: 55.5))
                 scrollView.addSubview(button)
+            }
         }
     }
 
